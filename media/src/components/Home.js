a300082f461a24aaaf2ea74ebc932ea9550402e4
Harsh Vardhan <vk5415308@gmail.com>
2022-11-04 13:21:07 +0530

    init

--- media/src/components/Home.js
+++ media/src/components/Home.js
@@ -4,9 +4,7 @@ import "../App.css";
 const Home = () => {
   return (
     <div className="background h-screen text-center  w-100 flex flex-col justify-center">
-      <h1 className="font-semibold text-5xl md:text-7xl lg:text-9xl">
-        MausamB
-      </h1>
+      <h1 className="font-semibold text-5xl md:text-7xl lg:text-9xl">VIDSIC</h1>
       <p>Baar Baar Dekho - Hazaar Baar Dekho </p>
 
       <div className="font-semibold uppercase  mt-5 flex  flex-col w-1/5 space-y-3 mx-auto">
